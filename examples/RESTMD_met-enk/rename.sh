12e07d93e16f4655350c8e3d2db7a2188af940c3
dstelter92 <dstelter@bu.edu>
2018-07-17 17:52:52 -0400

    example updates

--- examples/RESTMD_met-enk/rename.sh
+++ examples/RESTMD_met-enk/rename.sh
@@ -8,12 +8,12 @@ LAST=$2
 cp log.lammps log.lammps-$LAST
 for ((i=0; i<$NREPLICA; i++));
 do
-  cp WH.$i.d $i/WH.$i.d-$LAST
-  cp WT.$i.d $i/WT.$i.d-$LAST
-  cp WHP.$i.d $i/WHP.$i.d-$LAST
-  cp log.lammps.$i $i/log.lammps.$i-$LAST
-  cp oREST.$i.d $i/oREST.$i.d-$LAST
-  cp $i.dcd $i/$i-$LAST.dcd
+  cp WH.$i.d WH.$i.d-$LAST
+  cp WT.$i.d WT.$i.d-$LAST
+  cp WHP.$i.d WHP.$i.d-$LAST
+  cp log.lammps.$i log.lammps.$i-$LAST
+  cp oREST.$i.d oREST.$i.d-$LAST
+  #cp $i.dcd /$i-$LAST.dcd
 done
 
 sleep 2
--- examples/RESTMD_met-enk/submit.sh
+++ examples/RESTMD_met-enk/submit.sh
@@ -10,39 +10,41 @@
 # Script will restart itself, runs for total of 20 M steps
 # if the resubmission is uncommented. 
 
-NSLOTS=16
+NSLOTS=8
 NREPLICA=2
 
 LAST=$(cat last)
 PROCPERPART=$(($NSLOTS/$NREPLICA))
 PROC=${NREPLICA}x$PROCPERPART
 
+LMP=~/LAMMPS/lammps-dev/src/lmp_ubuntu.openmp
 
 # Run LAMMPS, be sure to edit the path!
 if [ $LAST -eq 1 ]
 then
-  mpirun -np $NSLOTS /path/to/lmp_exe -p $PROC -in run.in.restmd
-  ./rename.sh $NREPLICA $LAST
+  INP=run.in.restmd
 else
-  mpirun -np $NSLOTS /path/to/lmp_exe -p $PROC -in restart.in.restmd
-  ./rename.sh $NREPLICA $LAST
+  INP=restart.in.restmd
+fi
+
+mpirun -np $NSLOTS $LMP -p $PROC -i $INP
+if [ $? -ne 0 ]; then
+  echo "Simulation did not complete."
+  exit 1
 fi
 sleep 2
+ 
+./rename.sh $NREPLICA $LAST
 
 # Update Walkers
 tail -1 log.lammps | awk '{for (i=2; i<=NF; i++) print $i}' | tr '\n' ' ' > last_walkers
 sed -e "s/ZZZ/$(sed 's:/:\\/:g' last_walkers)/" restart.in.restmd.template > restart.in.restmd
+wait ${!}
+
 
 # Advance index counter
 LAST=$(($LAST + 1))
 echo $LAST > last
 sleep 2
-
-# (Uncomment to) Resubmit
-#if [ $LAST -lt 21 ]
-#then
-#  qsub submit.sh
-#  #./submit.sh
-#fi
 
 exit 0
